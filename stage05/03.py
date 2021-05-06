59348bec360ad6896e43a60d816eff2161cb743e
배지연 <bego9205@gmail.com>
2021-05-07 03:14:34 +0900

    solve

--- stage05/03.py
+++ stage05/03.py
@@ -8,14 +8,20 @@ while (mul != 0) :
     mul_int.append(mul % 10)
     mul //= 10
 
-i = 0
-mul_count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-while (mul_int[i]) :
+j = 0
+mul_count = []
+while (j < 10) :
 
-    index = mul_count.index(mul_int[i])
-    mul_count[index] += 1
-    i += 1
+    i = 0
+    num = 0
+    while (i < len(mul_int)) :
+        if (mul_int[i] == j) :
+            num += 1
+        i += 1
+    mul_count.append(num)
+    j += 1
 
 i = 0
 while (i < 10) :
     print(mul_count[i])
+    i += 1
