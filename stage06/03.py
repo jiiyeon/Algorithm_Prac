d13f00f4e8c2ecf6c6d46a742e8cbdbe8480f3f0
배지연 <bego9205@gmail.com>
2021-05-11 00:36:00 +0900

    solve

--- stage06/03.py
+++ stage06/03.py
@@ -1,8 +1,28 @@
-num_lst = list(input())
-i = 0
-while (i < len(num_lst)) :
-    num_lst[i] = int(num_lst[i])
-    i += 1
+N = int(input())
 
-def     arith_check(int) :
+def     arith_check(num1) :
+
+    num_lst = list(str(num1))
+    i = 0
+    res = 0
+    while (i < len(num_lst)) :
+        num_lst[i] = int(num_lst[i])
+        i += 1
+
+    if (len(num_lst) == 1) :
+        res += 1
+    elif (len(num_lst) == 2) :
+        res += 1
+    else :
+        if (num_lst[2] == num_lst[0] + (3 - 1) * (num_lst[1] - num_lst[0])) :
+            res += 1
     
+    return (res)
+
+i = 1
+cnt = 0
+while (i <= N) :
+    if (arith_check(i) == 1) :
+        cnt += 1
+    i += 1
+print(cnt)
