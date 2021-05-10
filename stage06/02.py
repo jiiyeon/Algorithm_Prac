ec13dfbee514f1944f358b2baac327d12e793e2c
배지연 <bego9205@gmail.com>
2021-05-10 23:47:03 +0900

    solve

--- stage06/02.py
+++ stage06/02.py
@@ -11,6 +11,15 @@ def     div_count(int) :
 
 i = 0
 int = 1
+decom_lst = []
 while (int <= 10000) :
-    print(div_count(int))
-    int = div_count(int)
+    decom_lst.append(div_count(int))
+    int += 1
+decom_lst = set(decom_lst)
+
+i = 0
+int = 1
+while (int <= 10000) :
+    if (int not in decom_lst) :
+        print(int)
+    int += 1
