f5148e123eab1309fb6d507bc403fdc10dd7cd32
배지연 <bego9205@gmail.com>
2021-05-19 21:57:10 +0900

    solve

--- stage09/01.py
+++ stage09/01.py
@@ -3,10 +3,15 @@ num_lst = list(map(int, input().split()))
 
 def     prime_check(int) :
 
-    n = 1
     flag = 1
-    while (n <= int) :
-        if ((int == 1) or (int / n == 0)) :
+
+    if (int == 1) :
+        flag = 0
+        return(flag)
+
+    n = 2
+    while (n < int) :
+        if (int % n == 0) :
             flag = 0
             break
         n += 1
@@ -16,6 +21,8 @@ def     prime_check(int) :
 i = 0
 cnt = 0
 while (i < size) :
+
+    print(prime_check(num_lst[i]))
     if (prime_check(num_lst[i]) == 1) :
         cnt += 1
     i += 1
