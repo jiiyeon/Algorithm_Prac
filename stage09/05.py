d0579c5480c8b7b37f3cfaaa701cfe438e31f0a1
배지연 <bego9205@gmail.com>
2021-05-27 01:54:17 +0900

    solve

--- stage09/05.py
+++ stage09/05.py
@@ -1,23 +1,23 @@
 #에라토스테네스의 체를 활용하여 소수 개수 집계
 def     ft_prime_lst(N) :
     #초기화 (1은 소수가 아니므로 False로 시작)
-    sieve = [False] + [True] * (N -  1)
+    sieve = [False] + [True] * (2 * N -  1)
 
     #최대 약수까지만 검사
-    mid = int(N ** 0.5)
+    mid = int((2 * N) ** 0.5)
     i = 1
     while (i < mid) :
         if (sieve[i] == True) :
 
             j = i + 1
-            while (i + j < N) :
+            while (i + j < 2 * N) :
                 sieve[i + j] = False
                 j += (i + 1)
         i += 1
     
     prime_lst = []
-    i = 1
-    while (i < N) :
+    i = N
+    while (i < 2 * N) :
         if (sieve[i] == True) :
             prime_lst.append(i + 1)
         i += 1
