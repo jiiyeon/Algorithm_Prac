101de0ca8748a988b8d32e80d5fa5cdbd7f12f6a
배지연 <bego9205@gmail.com>
2021-06-25 18:18:06 +0900

    solve

--- stage19/06.py
+++ stage19/06.py
@@ -1,1 +1,80 @@
+from collections import deque
 import sys
+
+size = int(sys.stdin.readline().rstrip())
+
+def     push_front(X):
+    global  deq
+
+    return deq.appendleft(X)
+
+def     push_back(X):
+    global  deq
+
+    return deq.append(X)
+
+def     pop_front():
+    global  deq
+
+    if not deq:
+        return (-1)
+    else:
+        return deq.popleft()
+
+def     pop_back():
+    global  deq
+
+    if not deq:
+        return (-1)
+    else:
+        return deq.pop()
+
+def     ft_size():
+    return len(deq)
+
+def     ft_empty():
+    if not deq:
+        return 1
+    else:
+        return 0
+
+def     ft_front():
+    if deq:
+        return deq[0]
+    else:
+        return -1
+
+def     ft_back():
+    if deq:
+        return deq[-1]
+    else:
+        return -1
+
+i = 0
+deq = deque([])
+print_lst = []
+while i < size:
+    sen = list(sys.stdin.readline().split())
+
+    if sen[0] == "push_front":
+        push_front(sen[1])
+    elif sen[0] == "push_back":
+        push_back(sen[1])
+    elif sen[0] == "pop_front":
+        print_lst.append((pop_front()))
+    elif sen[0] == "pop_back":
+        print_lst.append((pop_back()))
+    elif sen[0] == "size":
+        print_lst.append((ft_size()))
+    elif sen[0] == "empty":
+        print_lst.append((ft_empty()))
+    elif sen[0] == "front":
+        print_lst.append((ft_front()))
+    elif sen[0] == "back":
+        print_lst.append((ft_back()))
+    i += 1
+
+i = 0
+while i < len(print_lst):
+    print(print_lst[i])
+    i += 1
--- stage19/07.py
+++ stage19/07.py
@@ -1,1 +1,29 @@
+#조건1: size의 deque을 만들고, 주어진 리스트를 만들기 위해서 deque의 순서를 총 몇 번 변경해야하는지 구함 
+
 import sys
+from collections import deque
+
+size, last = map(int, sys.stdin.readline().split())
+value = list(map(int, sys.stdin.readline().split()))
+deq = deque([i for i in range(1, size + 1)])
+
+cnt = 0
+for idx in value:
+    while 1:
+        #구하고자하는 값이 deque의 맨 처음에 있는 경우
+        if deq[0] == idx:
+            deq.popleft()
+            break
+        #순서를 바꿔야하는 경우
+        else:
+            #구하고자하는 값의 index가 deque의 앞부분에 위치한 경우 : deque 맨 앞의 숫자를 pop
+            if deq.index(idx) < len(deq) / 2:
+                while deq[0] != idx:
+                    deq.append(deq.popleft())
+                    cnt += 1
+            #구하고자하는 값의 index가 deque의 뒷부분에 위치한 경우 : deque 맨 뒤의 숫자를 pop
+            else:
+                while deq[0] != idx:
+                    deq.appendleft(deq.pop())
+                    cnt += 1
+print(cnt)
