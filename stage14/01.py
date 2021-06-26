21c93da18b7286f2ee84d22bd88a8e2101854cc2
배지연 <bego9205@gmail.com>
2021-06-26 19:59:38 +0900

    solve

--- stage14/01.py
+++ stage14/01.py
@@ -1,1 +1,24 @@
+#조건 : 백트래킹 == dfs 기반으로 stack을 이용해 후진함. 브루트포스 전략에서 가지치기를 접목하는 것이 중요.
+
 import sys
+
+size, value = map(int, sys.stdin.readline().split())
+num_lst = [x for x in range(1, size + 1)]
+
+#case : stack으로 활용
+case = []
+def     dfs():
+    #case의 길이가 value가 되면 값을 print하고 반환함
+    if len(case) == value:
+        print(*case)
+        return
+
+    for i in num_lst:
+        #case에 이미 들어있을 경우 넘어감
+        if i in case:
+            continue
+        case.append(i)
+        dfs()
+        case.pop()
+
+dfs()
--- stage14/02.py
+++ stage14/02.py
@@ -1,1 +1,29 @@
+#조건 : 중복을 허락하지 않으므로 방문여부를 기록하는 배열이 필요
+
 import sys
+
+size, value = map(int, sys.stdin.readline().split())
+visited = [0 for _ in range(size)]
+
+#case는 stack으로 활용함
+case = []
+def     dfs_visit():
+    #case 원소의 개수가 value일 경우 return
+    if len(case) == value:
+        print(*case)
+        return
+
+    #visited와 함께 처리하기 위해서 index 기반으로 조건문/반복문 실행
+    for i in range(size):
+        #중복체크 : 방문하지 않은 수에 대해서
+        if visited[i] == 0:
+            visited[i] = 1
+            case.append(i + 1)
+            dfs_visit()
+            case.pop()
+
+            #case를 꺼낸 후 다음 부분만 확인하기 위해서 visited를 i + 1부터 초기화
+            for r in range(i + 1, size):
+                visited[r] = 0
+
+dfs_visit()
