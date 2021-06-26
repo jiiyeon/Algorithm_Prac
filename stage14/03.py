5737ac103318ea9d4e0bcdb0760511d4966e4b0a
배지연 <bego9205@gmail.com>
2021-06-26 20:29:43 +0900

    solve

--- stage14/03.py
+++ stage14/03.py
@@ -1,1 +1,16 @@
 import sys
+
+size, value = map(int, sys.stdin.readline().split())
+
+case = []
+def     dfs_plus():
+    if len(case) == value:
+        print(*case)
+        return
+    
+    for num in range(1, size + 1):
+        case.append(num)
+        dfs_plus()
+        case.pop()
+
+dfs_plus()
--- stage14/04.py
+++ stage14/04.py
@@ -1,1 +1,17 @@
 import sys
+
+size, value = map(int, sys.stdin.readline().split())
+
+case = []
+def     dfs_next(depth, start):
+
+    if depth == value:
+        print(*case)
+        return
+
+    for num in range(start, size + 1):
+        case.append(num)
+        dfs_next(depth + 1, num)
+        check_pt = case.pop()
+
+dfs_next(0, 1)
