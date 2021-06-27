89472613cd28f2c71898bbb9b178ab7d4e213c91
배지연 <bego9205@gmail.com>
2021-06-27 17:54:33 +0900

    solve

--- stage15/02.py
+++ stage15/02.py
@@ -1,1 +1,38 @@
+#조건 : 동적계획법에서 어떤 부분을 저장할 것인지 파악
+
 import sys
+
+#3차원 배열 선언
+# a, b, c가 20 이상일 경우 20이라고 줄이므로 dp의 최댓값은 (20,20,20)
+dp = [[[0] * 21 for _ in range(21)] for _ in range(21)]
+
+#주어진 점화식 구현
+def     w(a, b, c):
+    #조건1 : a 또는 b 또는 c가 음수일때
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    #조건2 : a 또는 b 또는 c가 20 초과일 때
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+
+    #memorization : (a, b, c)에 대하여 함수 w가 계산한 값이 이미 존재한다면 그 값을 불러옴
+    # 함수에서 계산이 완료된 후 그 값을 dp[a][b][c]에 저장하도록 구현함
+    if dp[a][b][c]:
+        return dp[a][b][c]
+
+    #memorizaion 조건문
+    if a < b < c:
+        dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+        return dp[a][b][c]
+    
+    #조건3 : w(a, b, c)를 수행했을때 계산되는 값
+    dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1 , b - 1, c - 1)
+    return dp[a][b][c]
+
+while 1:
+    a, b, c = map(int, sys.stdin.readline().split())
+
+    if all((a == -1, b == -1, c == -1)):
+        break
+
+    print("w({}, {}, {}) = {}".format(a, b, c, w(a, b, c)))
--- stage15/03.py
+++ stage15/03.py
@@ -1,1 +1,33 @@
+#조건 : N = (0 세트의 개수) * 2 + (1 카드의 개수)이므로 N = 2 * X + Y가 성립하는 (X, Y)를 구함
+# 각 순서쌍을 만족하는 조합의 수는 [x] * X + [y] * Y 원소를 나열하는 경우의 수와 같음
+# 즉 ∑(X + Y)! / (X! * Y!)
+
 import sys
+from types import resolve_bases
+
+N = int(sys.stdin.readline().rstrip())
+dp = [[0]* (N + 1) for _ in range(N + 1)]
+
+def     ft_factorial(n):
+
+    if n <= 1:
+        return 1
+    return n * ft_factorial(n - 1)
+
+i = 0
+res = 0
+while i <= N:
+
+    j = 0
+    while j <= N:
+
+        if dp[i][j]:
+            continue
+
+        elif (2 * i + j) == N:
+           dp[i][j] = ft_factorial(i + j) / (ft_factorial(i) * ft_factorial(j))
+           res += dp[i][j]
+        j += 1
+    i += 1
+
+print(res % 15746)
