fe5d70c74aaa5e498c77aff2dac1f4ccc09b518b
배지연 <bego9205@gmail.com>
2021-06-28 23:52:33 +0900

    solve

--- stage20/03.py
+++ stage20/03.py
@@ -1,1 +1,14 @@
+#조건 : 쿼드트리 문제풀이 방식으로 9분할면에 적용
+
 import sys
+
+def     solve(x, y, size):
+    global zero, minus, plus
+
+if __name__ = "__main__":
+    size = int(sys.stdin.readline().rstrip())
+    board = [list(map(int, sys.stdin.readline().split())) for _ in range(size)]
+
+    zero = 0
+    minus = 0
+    plus = 0
