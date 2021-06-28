36ca7df054741a6dc0de257f7b108468d86be7e4
배지연 <bego9205@gmail.com>
2021-06-28 16:32:58 +0900

    solve

--- stage16/04.py
+++ stage16/04.py
@@ -1,1 +1,20 @@
+#조건 : 결과값이 최소값이 되도록 ( )를 삽입하여 계산함
+# "-"에서 다음 "-"를 만날 때까지 ( )로 묶음
+
 import sys
+
+sen = sys.stdin.readline().split("-")
+res = 0
+
+#case1 : "-"부호 기준 앞부분
+for item01 in sen[0].split("+"):
+    res += int(item01)
+
+#case2 : "-"부호 기준 뒷부분
+# "-"가 여러번 등장했을 경우 sen[1:]에 해당하는 원소가 여러개일 수 있음
+# 각 원소는 -( )형태이므로 "+"가 있는 경우 ( )안의 값을 꺼내서 각각 뺌
+for item02 in sen[1:]:
+    for sub_item02 in item02.split("+"):
+        res -= int(sub_item02)
+
+print(res)
--- stage16/05.py
+++ stage16/05.py
@@ -1,1 +1,25 @@
+#조건 : 기름값 예산을 최소한으로 책정하기
+# A → B → C로 갈 때, B도시에 도착했는데 A도시의 기름값이 더 쌀 경우 A도시의 기름값으로 예산을 책정하면됨. (코드에서 돌아가는 것이 아님)
+
 import sys
+
+def     solve():
+    res = 0
+    oilMin = oil_lst[0]
+
+    for i in range(size - 1):
+
+        #현재 도시의 기름값과 지금까지 기름값 중 최솟값을 비교하여 현재도시가 더 작으면 최솟값 변경
+        # 이전 최솟값이 더 작을 경우 최솟값인 도시에서 i → (i + 1) 경로에서 필요한 기름을 충전하는 것으로 예산 책정
+        if oilMin > oil_lst[i]:
+            oilMin = oil_lst[i]
+        res += (oilMin * path_lst[i])
+    
+    return res
+
+if __name__ == "__main__":
+    size = int(sys.stdin.readline().rstrip())
+    path_lst = list(map(int, sys.stdin.readline().split()))
+    oil_lst = list(map(int, sys.stdin.readline().split()))
+
+    print(solve())
