547104faac7be2cd14ef394055fb9a0d093cb598
배지연 <bego9205@gmail.com>
2021-06-28 15:04:10 +0900

    solve

--- stage16/02.py
+++ stage16/02.py
@@ -1,1 +1,34 @@
+#조건 : 회의가 가장 많이 열릴 수 있도록 일정 구성 (i번째의 회의시간이 짧고, i번째에서 i + 1 번째로 넘어가는 간격이 짧아야함)
+# 회의가 빨리 끝나는 기준으로 선택하면, 남은 시간이 더 길어지므로 고려할 수 있는 회의 수가 늘어남
+# 따라서 회의시각을 key1 : 회의 종료 시각, key2 : 회의 시작 시각으로 정렬하여 순서 그대로 cnt
+
 import sys
+
+def     solve(table):
+    cnt = 0
+    start = 0
+
+    #정렬된 회의 시간표 순서쌍(start, end)에서
+    for time in table:
+
+        #만약 time이 가능한 시작시점과 같이(또는 늦게) 시작한다면 time을 포함하고
+        # 회의 시작 가능한 시각을 time이 끝나는 시각으로 변경함
+        if time[0] >= start:
+            start = time[1]
+            cnt += 1
+    
+    return cnt
+
+#시작함수
+if __name__ == "__main__":
+    size = int(sys.stdin.readline().rstrip())
+    table = []
+
+    for i in range(size):
+        start, end = map(int, sys.stdin.readline().split())
+        table.append((start, end))
+    
+    #key1 : 종료시각, key2 : 시작시각
+    # sorted에서 key가 여러 개 사용될 경우 tuple 사용
+    table = sorted(table, key=lambda x: (x[1], x[0]))
+    print(solve(table))
--- stage16/03.py
+++ stage16/03.py
@@ -1,1 +1,26 @@
+#조건1 : 각 사람들이 현금을 인출하기 위해 소요한 시간 = 앞 사람들의 처리시간 + 자신의 처리시간
+#조건2 : 기계가 1대이므로 대기시간이 누적됨
+# 즉 앞사람들의 처리시간이 짧아야 누적되는 값이 작아질 수 있으므로 처리시간이 짧은 사람부터 업무를 보도록 정리
+
 import sys
+
+def     solve(arr):
+    total = 0
+    wait = 0
+
+    #total은 모든 사람이 소요한 시간의 합
+    # 따라서 각 사람이 자신의 차례가 되었을때, 자신의 차례가 되기까지 걸린 시간(wait)과 자신의 업무시간(time)을 total에 추가함
+    for time in arr:
+        total += (wait + time)
+        wait += time
+    
+    return total
+
+#시작함수
+if __name__ == "__main__":
+
+    size = int(sys.stdin.readline().rstrip())
+    arr = list(map(int, sys.stdin.readline().split()))
+    arr = sorted(arr)
+
+    print(solve(arr))
